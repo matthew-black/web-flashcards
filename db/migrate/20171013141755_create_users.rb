94cc44b058e8a81efc0d56a80001b5a66a157c2d
Matthew Black & Mitch Mele  & Patrick Rodriguez <pair+matthew-black.msmele345.patrodriguez108@devbootcamp.com>
2017-10-13 15:19:32 -0500

    add registration, logins.

--- db/migrate/20171013141755_create_users.rb
+++ db/migrate/20171013141755_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[5.0]
     create_table :users do |t|
       t.string :username, null: false, unique: true
       t.string :email, null: false, unique: true
-      t.string :password, null: false
+      t.string :encrypted_password, null: false
 
 
       t.timestamps
---
+++ db/seeds.rb
@@ -0,0 +1,22 @@
+require 'faker'
+
+
+# 5.times do
+#   card = {:question => Faker::Pokemon.name, :answer => Faker::Pokemon.location, :deck_id => 1}
+
+#   Card.create!(card)
+# end
+
+
+# Deck.create!(:name=> "Pokemon")
+
+
+
+# 5.times do
+#   user = { :username => Faker::Internet.user_name, :email => Faker::Internet.email, :password => "password" }
+#   User.create!(user)
+# end
+
+
+# Round.create!(:user_id => 1, :deck_id => 1)
+
