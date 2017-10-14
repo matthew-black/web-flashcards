e97daa7d176b1676ca6594022756e7e7c0df44b7
Matthew Black & Mitch Mele  & Patrick Rodriguez <pair+matthew-black.msmele345.patrodriguez108@devbootcamp.com>
2017-10-14 17:51:26 -0500

    Clean up routing.

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -5,10 +5,8 @@ end
 
 get '/decks/:id' do
   @deck = Deck.find(params[:id])
-  session[:round_id] = Round.create(user_id: session[:user_id], deck_id: params[:id]).id
   erb :'decks/show'
 end
-
 
 get '/sessions-view' do
   session.inspect
@@ -17,11 +15,3 @@ end
 get '/sessions-clear' do
   session.clear
 end
-
-  # deck = Round.find(session[:round]).deck_id
-  # all_card_ids = deck.cards.pluck(:id)
-  # solved_card_ids = @round.guesses.select { |guess| guess.is_correct }
-  # card_ids = all_card_ids - solved_card_ids
-  # ep card_ids
-  # @card = Card.find_by(id: card_ids.sample)
-  # ep @card
--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -1,4 +1,7 @@
-
+post '/decks/:id/rounds' do
+  session[:round_id] = Round.create(user_id: session[:user_id], deck_id: params[:id]).id
+  redirect "/rounds/#{session[:round_id]}"
+end
 
 get '/rounds/:id' do
   deck_id = Round.find(session[:round_id]).deck_id
@@ -6,24 +9,15 @@ get '/rounds/:id' do
   round = Round.find(session[:round_id])
 
   all_card_ids = deck.cards.pluck(:id)
-  ep all_card_ids
 
   successful_guesses = round.guesses.select { |guess| guess.is_correct }
-  ep successful_guesses
 
   solved_card_ids = successful_guesses.map { |guess| guess.card_id }
-  ep solved_card_ids
 
   card_ids = all_card_ids - solved_card_ids
 
-
   @card = Card.find_by(id: card_ids.sample)
+
   erb:'rounds/show'
 end
 
-
-# post route
-
-# look at provided answer
-# create guess object in table
-#
