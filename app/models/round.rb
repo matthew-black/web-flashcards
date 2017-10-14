0712ee7d379b51b92eab31ce4f2c0f540aaa0811
Matthew Black & Mitch Mele  & Patrick Rodriguez <pair+matthew-black.msmele345.patrodriguez108@devbootcamp.com>
2017-10-14 18:17:14 -0500

    Initial refactor to pull the mess out of our rounds route.

--- app/models/round.rb
+++ app/models/round.rb
@@ -2,4 +2,12 @@ class Round < ApplicationRecord
   belongs_to :user
   belongs_to :deck
   has_many :guesses
+
+  def prepare_card
+    all_card_ids = self.deck.cards.pluck(:id)
+    solved_guesses = self.guesses.select { |guess| guess.is_correct }
+    solved_card_ids = solved_guesses.map { |guess| guess.card_id }
+    remaining_card_ids = all_card_ids - solved_card_ids
+    Card.find(remaining_card_ids.sample)
+  end
 end
